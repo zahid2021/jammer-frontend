1c06cea487b875b0cf91edf346490746188ebd6b
Your Name <your-email@example.com>
2025-05-01 23:45:46 -0700

    Add remaining git script files

---
+++ commit-push.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Git\bin\git.exe" commit -m "Add git setup scripts"
+"C:\Program Files\Git\bin\git.exe" push
+pause 
---
+++ final-commit.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\Program Files\Git\bin\git.exe" add commit-push.bat
+"C:\Program Files\Git\bin\git.exe" add final-commit.bat
+"C:\Program Files\Git\bin\git.exe" commit -m "Add remaining git script files"
+"C:\Program Files\Git\bin\git.exe" push
+pause 
